66254497a08b46855e90ff14823df6990755fbb0
slow <73202350+slow@users.noreply.github.com>
2021-11-30 03:11:09 +0000

    plugin cards revamp

--- src/renderer/ui/components/addoncard.tsx
+++ src/renderer/ui/components/addoncard.tsx
@@ -28,7 +28,7 @@ export function ToolButton({label, icon, onClick, danger = false, disabled = fal
                     <Icon name={icon} color={danger ? "#ed4245" : void 0} width="20" height="20" />
                 </Button>
             )}
-        </Tooltip>  
+        </Tooltip>
     );
 };
 
@@ -61,35 +61,33 @@ export default function AddonCard({addon, manager, openSettings, hasSettings, ty
     }, [addon, manager]);
 
     return (
-        <div className={"pc-settings-addon-card " + addon.manifest.name?.replace(/ /g, "-")}>
-            <div className="pc-settings-card-tools">
-                <ToolButton label="Reload" icon="Replay" onClick={() => manager.reload(addon)} disabled />
-                <ToolButton label="Open Path" icon="Folder" onClick={() => {
-                    PCCompatNative.executeJS(`require("electron").shell.showItemInFolder(${JSON.stringify(addon.path)})`);
-                }} disabled />
-                <ToolButton label="Delete" icon="Trash" danger onClick={() => {
-                    Modals.showConfirmationModal("Are you sure?", `Are you sure that you want to delete the ${type} "${addon.manifest.name}"?`, {
-                        onConfirm: () => {
-                            PCCompatNative.executeJS(`require("electron").shell.trashItem(${JSON.stringify(addon.path)})`);
-                        }
-                    });
-                }} />
-            </div>
+        <div style={{ "--plugin-color": addon.color }} className={"pc-settings-addon-card " + addon.manifest.name?.replace(/ /g, "-")}>
             <div className="pc-settings-card-header">
                 <div className="pc-settings-card-field pc-settings-card-name">{addon.manifest.name}</div>
                 {"version" in addon.manifest && <div className="pc-settings-card-field">v{addon.manifest.version}</div>}
                 {"author" in addon.manifest && <div className="pc-settings-card-field"> by {addon.manifest.author}</div>}
+                <div className="pc-settings-card-controls">
+                  <ToolButton label="Reload" icon="Replay" disabled={!manager.isEnabled?.(addon) ?? true} onClick={() => manager.reload(addon)} />
+                  <ToolButton label="Open Path" icon="Folder" onClick={() => {
+                     PCCompatNative.executeJS(`require("electron").shell.showItemInFolder(${JSON.stringify(addon.path)})`);
+                  }} />
+                  <ToolButton label="Delete" icon="Trash" onClick={() => {
+                     Modals.showConfirmationModal("Are you sure?", `Are you sure that you want to delete the ${type} "${addon.manifest.name}"?`, {
+                           onConfirm: () => {
+                              PCCompatNative.executeJS(`require("electron").shell.trashItem(${JSON.stringify(addon.path)})`);
+                           }
+                     });
+                  }} />
+                  <ButtonWrapper value={manager.isEnabled?.(addon) ?? false} onChange={() => {
+                     manager.toggle(addon);
+                  }} />
+                </div>
             </div>
             {addon.manifest.description && (
                 <div className="pc-settings-card-desc">
                     <Markdown>{addon.manifest.description}</Markdown>
                 </div>
             )}
-            <div className="pc-settings-card-footer">
-                <ButtonWrapper value={manager.isEnabled?.(addon) ?? false} onChange={() => {
-                    manager.toggle(addon);
-                }} />
-            </div>
         </div>
     );
 }
