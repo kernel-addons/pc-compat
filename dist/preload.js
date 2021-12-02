5fb9b72f63efa86fadde3e6b045f8f6a182ee700
slow <73202350+slow@users.noreply.github.com>
2021-12-02 02:33:04 +0000

    Squashed commit of the following:
    commit 2db1ae3a81e3ac231d33e0e1d7b95f595048a15e
    Author: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Wed Dec 1 21:38:58 2021 +0000
    
        Build.
    
    commit de1810c78e250a6a4aee3a960f51f5d847e2d8dc
    Merge: b7d0615 077f536
    Author: Strencher <46447572+Strencher@users.noreply.github.com>
    Date:   Wed Dec 1 22:38:31 2021 +0100
    
        Merge pull request #1 from strencher-kernel/dev
    
    commit 077f536c39adf469ae6fe34519a7a20cc190feff
    Merge: 65eaf6a b7d0615
    Author: Strencher <46447572+Strencher@users.noreply.github.com>
    Date:   Wed Dec 1 22:38:00 2021 +0100
    
        Merge branch 'master' into dev
    
    commit 65eaf6a6788bbc4fa9ce14aac1e45cf110f613a6
    Author: Strencher <maxarms1@gmail.com>
    Date:   Wed Dec 1 22:33:45 2021 +0100
    
        Finalize webpack.
    
    commit b7d0615ce5749a623bc5974278a972a194e270a2
    Author: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Date:   Tue Nov 23 17:00:29 2021 +0000
    
        Build.

--- dist/preload.js
+++ dist/preload.js
@@ -15,6 +15,7 @@ var path__default = /*#__PURE__*/ _interopDefaultLegacy(path);
 
 // Main
 const MAIN_EVENT = "pccompat-main-event";
+const GET_APP_PATH = "pccompat-get-app-path";
 // Preload
 const EXPOSE_PROCESS_GLOBAL = "pccompat-expose-process-global";
 
@@ -72,12 +73,16 @@ function cloneObject(target, newObject = {
 	}, newObject);
 }
 
+var ref1;
 const nodeModulesPath = path__default["default"].resolve(process.cwd(), "resources", "app-original.asar", "node_modules");
 // @ts-ignore - Push modules
 if (!Module__default["default"].globalPaths.includes(nodeModulesPath)) Module__default["default"].globalPaths.push(nodeModulesPath);
 const API = {
 	getAppPath() {
-		return;
+		return electron.ipcRenderer.sendSync(GET_APP_PATH);
+	},
+	getBasePath() {
+		return path__default["default"].resolve(__dirname, "..");
 	},
 	executeJS(js) {
 		return eval(js);
@@ -97,10 +102,23 @@ Object.defineProperties(window, {
 		writable: false
 	}
 });
-electron.contextBridge.exposeInMainWorld("PCCompatNative", API);
+if ((electron.webFrame === null || electron.webFrame === void 0 ? void 0 : (ref1 = electron.webFrame.top) === null || ref1 === void 0 ? void 0 : ref1.context) != null) {
+	electron.webFrame.top.context.window.PCCompatNative = API;
+	electron.webFrame.top.context.window.require = require;
+	electron.webFrame.top.context.window.Buffer = Buffer;
+} else {
+	electron.contextBridge.exposeInMainWorld("PCCompatNative", API);
+}
 IPC.on(EXPOSE_PROCESS_GLOBAL, () => {
 	try {
-		electron.contextBridge.exposeInMainWorld("process", cloneObject(process));
+		var ref;
+		if (!process.contextIsolated) {
+			window.process = cloneObject(process);
+		} else if ((electron.webFrame === null || electron.webFrame === void 0 ? void 0 : (ref = electron.webFrame.top) === null || ref === void 0 ? void 0 : ref.context) != null) {
+			electron.webFrame.top.context.window.process = cloneObject(process);
+		} else {
+			electron.contextBridge.exposeInMainWorld("process", cloneObject(process));
+		}
 	} catch (error) {
 		error.name = "NativeError";
 		console.error("Failed to expose process global:", error);
