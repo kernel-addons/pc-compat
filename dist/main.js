cfc36dfa6bad7b6e70d96ca91204819ac7b32ba8
slow <73202350+slow@users.noreply.github.com>
2021-12-08 03:55:15 +0000

    Build.

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,24 @@
-"use strict";
+'use strict';
 
-var Module = require("module");
-var path = require("path");
-var inspector = require("inspector");
-var sucrase = require("sucrase");
-var sass = require("sass");
-var electron = require("electron");
-var fs = require("fs");
+var Module = require('module');
+var path = require('path');
+var inspector = require('inspector');
+var sucrase = require('sucrase');
+var sass = require('sass');
+var electron = require('electron');
+var fs = require('fs');
 
-function _interopDefaultLegacy(e) {
-	return e && typeof e === "object" && "default" in e ? e : {
-		"default": e
-	};
-}
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var Module__default = /*#__PURE__*/ _interopDefaultLegacy(Module);
-var path__default = /*#__PURE__*/ _interopDefaultLegacy(path);
-var sucrase__default = /*#__PURE__*/ _interopDefaultLegacy(sucrase);
-var sass__default = /*#__PURE__*/ _interopDefaultLegacy(sass);
-var fs__default = /*#__PURE__*/ _interopDefaultLegacy(fs);
+var Module__default = /*#__PURE__*/_interopDefaultLegacy(Module);
+var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
+var sucrase__default = /*#__PURE__*/_interopDefaultLegacy(sucrase);
+var sass__default = /*#__PURE__*/_interopDefaultLegacy(sass);
+var fs__default = /*#__PURE__*/_interopDefaultLegacy(fs);
 
 if (process.argv.join("").includes("--debug")) {
-	inspector.open();
-	inspector.waitForDebugger();
+    inspector.open();
+    inspector.waitForDebugger();
 }
 
 // Main
@@ -31,47 +27,48 @@ const COMPILE_JSX = "pccompat-compile-jsx";
 const GET_APP_PATH = "pccompat-get-app-path";
 const SET_DEV_TOOLS = "pccompat-open-devtools";
 
-electron.ipcMain.on(GET_APP_PATH, (event) => {
-	event.returnValue = electron.app.getAppPath();
+electron.ipcMain.on(GET_APP_PATH, (event)=>{
+    event.returnValue = electron.app.getAppPath();
 });
-electron.ipcMain.on(COMPILE_SASS, (event, file) => {
-	let result = "";
-	try {
-		// @ts-ignore
-		let abc = sass__default["default"].renderSync({
-			file
-		});
-		result = abc.css.toString();
-	} catch (error) {
-		console.error(error);
-	}
-	event.returnValue = result;
+electron.ipcMain.on(COMPILE_SASS, (event, file)=>{
+    let result = "";
+    try {
+        // @ts-ignore
+        let abc = sass__default["default"].renderSync({
+            file
+        });
+        result = abc.css.toString();
+    } catch (error) {
+        console.error(error);
+    }
+    event.returnValue = result;
 });
-electron.ipcMain.on(COMPILE_JSX, (event, file) => {
-	if (!fs__default["default"].existsSync(file)) {
-		event.returnValue = "";
-		return;
-	}
-	const filecontent = fs__default["default"].readFileSync(file, "utf8");
-	const {code} = sucrase__default["default"].transform(filecontent, {
-		transforms: [
-			"jsx",
-			"imports",
-			"typescript"
-		],
-		filePath: file
-	});
-	event.returnValue = code;
+electron.ipcMain.on(COMPILE_JSX, (event, file)=>{
+    if (!fs__default["default"].existsSync(file)) {
+        event.returnValue = "";
+        return;
+    }
+    const filecontent = fs__default["default"].readFileSync(file, "utf8");
+    const { code  } = sucrase__default["default"].transform(filecontent, {
+        transforms: [
+            "jsx",
+            "imports",
+            "typescript"
+        ],
+        filePath: file
+    });
+    event.returnValue = code;
 });
-electron.ipcMain.handle(SET_DEV_TOOLS, (event, value) => {
-	const win = electron.BrowserWindow.fromWebContents(event.sender);
-	if (!win) return;
-	if (value && !win.webContents.isDevToolsOpened()) {
-		win.webContents.openDevTools();
-	} else {
-		win.webContents.closeDevTools();
-	}
+electron.ipcMain.handle(SET_DEV_TOOLS, (event, value)=>{
+    const win = electron.BrowserWindow.fromWebContents(event.sender);
+    if (!win) return;
+    if (value && !win.webContents.isDevToolsOpened()) {
+        win.webContents.openDevTools();
+    } else {
+        win.webContents.closeDevTools();
+    }
 });
 
 // @ts-ignore
 Module__default["default"].globalPaths.push(path__default["default"].resolve(__dirname, "node_modules"));
+//# sourceMappingURL=main.js.map
--- dist/preload.js
+++ dist/preload.js
@@ -1,17 +1,13 @@
-"use strict";
+'use strict';
 
-var electron = require("electron");
-var Module = require("module");
-var path = require("path");
+var electron = require('electron');
+var Module = require('module');
+var path = require('path');
 
-function _interopDefaultLegacy(e) {
-	return e && typeof e === "object" && "default" in e ? e : {
-		"default": e
-	};
-}
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var Module__default = /*#__PURE__*/ _interopDefaultLegacy(Module);
-var path__default = /*#__PURE__*/ _interopDefaultLegacy(path);
+var Module__default = /*#__PURE__*/_interopDefaultLegacy(Module);
+var path__default = /*#__PURE__*/_interopDefaultLegacy(path);
 
 // Main
 const MAIN_EVENT = "pccompat-main-event";
@@ -23,105 +19,101 @@ const EXPOSE_PROCESS_GLOBAL = "pccompat-expose-process-global";
 const events = {
 };
 const IPC = {
-	on(event, callback) {
-		if (!events[event])
-			events[event] = new Set();
-		return events[event].add(callback), IPC.off.bind(null, event, callback);
-	},
-	off(event, callback) {
-		if (!events[event]) return;
-		events[event].delete(callback);
-	},
-	once(event, callback) {
-		const unsubscribe = IPC.on(event, (...args) => {
-			unsubscribe();
-			return callback(...args);
-		});
-	},
-	dispatch(event, ...args) {
-		if (!events[event]) return;
-		for (const callback of events[event]) {
-			try {
-				callback(...args);
-			} catch (error) {
-				console.error(error);
-			}
-		}
-	},
-	sendMain(event, ...args) {
-		return electron.ipcRenderer.sendSync(MAIN_EVENT, event, ...args);
-	}
+    on (event, callback) {
+        if (!events[event]) events[event] = new Set();
+        return events[event].add(callback), IPC.off.bind(null, event, callback);
+    },
+    off (event, callback) {
+        if (!events[event]) return;
+        events[event].delete(callback);
+    },
+    once (event, callback) {
+        const unsubscribe = IPC.on(event, (...args)=>{
+            unsubscribe();
+            return callback(...args);
+        });
+    },
+    dispatch (event, ...args) {
+        if (!events[event]) return;
+        for (const callback of events[event]){
+            try {
+                callback(...args);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    sendMain (event, ...args) {
+        return electron.ipcRenderer.sendSync(MAIN_EVENT, event, ...args);
+    }
 };
 
 function getKeys(object) {
-	const keys = [];
-	for (const key in object) keys.push(key);
-	return keys;
+    const keys = [];
+    for(const key in object)keys.push(key);
+    return keys;
 }
 function cloneObject(target, newObject = {
-	}, keys) {
-	if (!Array.isArray(keys))
-		keys = getKeys(target);
-	return keys.reduce((clone, key) => {
-		if (typeof target[key] === "object" && !Array.isArray(target[key]) && target[key] !== null)
-			clone[key] = cloneObject(target[key], {
-			});
-		else if (typeof target[key] === "function")
-			clone[key] = target[key].bind(target);
-		else
-			clone[key] = target[key];
-		return clone;
-	}, newObject);
+}, keys) {
+    if (!Array.isArray(keys)) keys = getKeys(target);
+    return keys.reduce((clone, key)=>{
+        if (typeof target[key] === "object" && !Array.isArray(target[key]) && target[key] !== null) clone[key] = cloneObject(target[key], {
+        });
+        else if (typeof target[key] === "function") clone[key] = target[key].bind(target);
+        else clone[key] = target[key];
+        return clone;
+    }, newObject);
 }
 
 const nodeModulesPath = path__default["default"].resolve(process.cwd(), "resources", "app-original.asar", "node_modules");
 // @ts-ignore - Push modules
 if (!Module__default["default"].globalPaths.includes(nodeModulesPath)) Module__default["default"].globalPaths.push(nodeModulesPath);
 const API = {
-	getAppPath() {
-		return electron.ipcRenderer.sendSync(GET_APP_PATH);
-	},
-	getBasePath() {
-		return path__default["default"].resolve(__dirname, "..");
-	},
-	executeJS(js) {
-		return eval(js);
-	},
-	setDevtools(opened) {
-		return electron.ipcRenderer.invoke(SET_DEV_TOOLS, opened);
-	},
-	IPC: IPC
+    getAppPath () {
+        return electron.ipcRenderer.sendSync(GET_APP_PATH);
+    },
+    getBasePath () {
+        return path__default["default"].resolve(__dirname, "..");
+    },
+    executeJS (js) {
+        return eval(js);
+    },
+    setDevtools (opened) {
+        return electron.ipcRenderer.invoke(SET_DEV_TOOLS, opened);
+    },
+    IPC: IPC
 };
 // Expose Native bindings and cloned process global.
 Object.defineProperties(window, {
-	PCCompatNative: {
-		value: Object.assign({
-		}, API, {
-			cloneObject,
-			getKeys
-		}),
-		configurable: false,
-		writable: false
-	},
-	PCCompatEvents: {
-		value: events,
-		configurable: false,
-		writable: false
-	}
+    PCCompatNative: {
+        value: Object.assign({
+        }, API, {
+            cloneObject,
+            getKeys
+        }),
+        configurable: false,
+        writable: false
+    },
+    PCCompatEvents: {
+        value: events,
+        configurable: false,
+        writable: false
+    }
 });
 electron.contextBridge.exposeInMainWorld("PCCompatNative", API);
-IPC.on(EXPOSE_PROCESS_GLOBAL, () => {
-	try {
-		if (!process.contextIsolated) {
-			Object.defineProperty(window, "process", {
-				value: cloneObject(process),
-				configurable: true
-			});
-		} else {
-			electron.contextBridge.exposeInMainWorld("process", cloneObject(process));
-		}
-	} catch (error) {
-		error.name = "NativeError";
-		console.error("Failed to expose process global:", error);
-	}
+IPC.on(EXPOSE_PROCESS_GLOBAL, ()=>{
+    try {
+        if (!process.contextIsolated) {
+            Object.defineProperty(window, "process", {
+                value: cloneObject(process),
+                configurable: true
+            });
+        } else {
+            electron.contextBridge.exposeInMainWorld("process", cloneObject(process));
+        }
+    } catch (error) {
+        error.name = "NativeError";
+        console.error("Failed to expose process global:", error);
+    }
 });
+//# sourceMappingURL=preload.js.map
