3dc88697bb42b43b47fcff2094a51a0318e4ac5a
slow <73202350+slow@users.noreply.github.com>
2022-01-02 15:50:32 +0000

    Polyfill connections API

---
+++ src/renderer/powercord/api/connections.ts
@@ -0,0 +1,15 @@
+export function fetchAccounts() {
+    return Promise.resolve([])
+}
+
+export const connections = [];
+
+export const filter = () => ([]);
+
+export const map = () => ([]);
+
+export function registerConnection() {}
+
+export function unregisterConnection() {}
+
+export function get() { return null }
