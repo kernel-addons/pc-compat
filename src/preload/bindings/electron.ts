b00e520b0b86b8f57ab88353c91393510c246a2f
Strencher <46447572+Strencher@users.noreply.github.com>
2022-07-01 22:09:11 +0200

    Implement ipcRenderer._events internal

--- src/preload/bindings/electron.ts
+++ src/preload/bindings/electron.ts
@@ -10,8 +10,29 @@ export type ElectronAPI = {
     contextBridge: Electron.ContextBridge,
 };
 
+const ipcRenderer = cloneObject(electron.ipcRenderer) as unknown as Electron.IpcRenderer;
+
 export const newElectron: ElectronAPI = contextIsolated ? {
-    ipcRenderer: cloneObject(electron.ipcRenderer) as unknown as Electron.IpcRenderer,
+    ipcRenderer: Object.assign(ipcRenderer, {
+        _events: null,
+        _listeners() {return Object.keys((electron.ipcRenderer as any)._events);},
+        _isArray(event) {
+            return Array.isArray((electron.ipcRenderer as any)._events[event]);
+        },
+        removeListener(_, listener) {
+            if (listener.$$type !== Symbol.for("PC_IPC_RENDERER_LISTENER")) {
+                throw new Error("Listener is not a valid electron listener.");
+            }
+            const {event, index} = listener;
+
+            const events = electron.ipcRenderer.listeners(event);
+
+            electron.ipcRenderer.removeListener(event, events[index] as (...args: any[]) => void);
+        },
+        removeAllListeners(channel) {
+            electron.ipcRenderer.removeAllListeners(channel);
+        }
+    }),
     shell: electron.shell,
     clipboard: electron.clipboard,
     contextBridge: electron.contextBridge
--- src/renderer/node/electron.ts
+++ src/renderer/node/electron.ts
@@ -1,3 +1,29 @@
 type ElectronModule = typeof import("src/preload/bindings/electron").default;
 
-export default PCCompatNative.getBinding("electron") as ElectronModule;
+const electron = PCCompatNative.getBinding("electron") as ElectronModule;
+
+(electron.ipcRenderer as any)._events = new Proxy({}, {
+    get(_, property) {
+        const isArray = (electron.ipcRenderer as any)._isArray(property);
+
+        if (isArray) {
+            return new Proxy({}, {
+                get(_, index) {
+                    return {
+                        $$type: Symbol.for("PC_IPC_RENDERER_LISTENER"),
+                        index,
+                        event: property
+                    }
+                }
+            });
+        }
+
+        return {
+            $$type: Symbol.for("PC_IPC_RENDERER_LISTENER"),
+            index: 0,
+            event: property
+        };
+    }
+});
+
+export default electron;
