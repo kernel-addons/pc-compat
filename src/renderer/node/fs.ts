798fd9f08bcd3675ba129124fe279f863f6685c2
Strencher <46447572+Strencher@users.noreply.github.com>
2022-08-08 21:28:07 +0200

    Fix readFileSync to return actual Buffer instance

--- src/renderer/node/fs.ts
+++ src/renderer/node/fs.ts
@@ -1,3 +1,15 @@
 type FsModule = typeof import("src/preload/bindings/fs").default;
 
-export default !window.process || process.contextIsolated ? PCCompatNative.getBinding("fs") as FsModule : window.require("fs");
+const fs = !window.process || process.contextIsolated ? PCCompatNative.getBinding("fs") as FsModule : window.require("fs");
+
+fs.readFileSync = (readFileSync => (...args) => {
+    let res = readFileSync(...args);
+
+    if (res instanceof Uint8Array) {
+        res = Buffer.from(res);
+    }
+
+    return res;
+})(fs.readFileSync);
+
+export default fs;
